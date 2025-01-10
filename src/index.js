d67b7d195879c963c43c1c07b85613890b24b45d
Muhammad Awais <awaismahaar499@gmail.com>
2025-01-10 17:12:32 +0500

    Add Custom Classes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
-import express from "express";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config();
-const app = express();
-(async ()=>{
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-       console.log("Connected to MongoDB");
-       app.on("error" , ()=>{
-        console.error("Error connecting to MongoDB:", error);
-       })
-       app.listen(process.env.PORT, ()=>{
-        console.log(`Server running on port ${process.env.PORT}`);
-       });
-    } catch (error) {
-     console.log("Error: " + error);
-      throw error;  
-    }
-})() 
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("Connected to MongoDB");
+    app.on("error", () => {
+      console.error("Error connecting to MongoDB:", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+    throw error;
+  }
+})();
---
+++ utils/asyncHandler.js
@@ -0,0 +1,19 @@
+//  const asyncHandler = (func) => async(req,res,next)=>{
+//      try {
+//         await func(req,res,next);
+//      } catch (error) {
+//         res.status(500).json({
+//            success : false,
+//            message : error.message,
+//         })
+
+//      }
+//  }
+
+const asyncHandler = (func) => {
+    async (req, res, next) => {
+    Promise.resolve(func(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
